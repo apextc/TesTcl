0df4863f9f365b978d2b612b935eaa54fbf076d9
apextc <andy@apextc.co.uk>
2018-11-12 10:06:13 +0000

    added b64encode, b64decode, ltm and setvar to global.tcl

--- src/global.tcl
+++ src/global.tcl
@@ -6,9 +6,9 @@ namespace eval ::testcl {
   # namespace export active_members
   # namespace export active_nodes
   # namespace export after
-  # namespace export b64decode
-  # namespace export b64encode
-  # namespace export call
+  namespace export b64decode
+  namespace export b64encode
+  namespace export call
   # namespace export CATEGORY::lookup
   # namespace export class
   # namespace export client_addr
@@ -17,11 +17,11 @@ namespace eval ::testcl {
   # namespace export clone
   # namespace export cpu
   # namespace export crc32
-  # namespace export decode_uri
+  namespace export decode_uri
   # namespace export DEMANGLE::disable
   # namespace export DEMANGLE::enable
   # namespace export discard
-  # namespace export domain
+  namespace export domain
   # namespace export drop
   # namespace export event
   # namespace export findclass
@@ -101,6 +101,116 @@ namespace eval ::testcl {
   # namespace export whereis
 }
 
+# testcl::b64decode --
+#
+# stub for the iRule GLOBAL::b64decode - decodes base64 string
+#
+# Arguments:
+# string
+#
+# Side Effects:
+# None.
+#
+# Results:
+# decoded string
+#
+# Usage syntax:
+# b64decode <string>
+proc ::testcl::b64decode {str} {
+  log::log debug "GLOBAL::b64decode $str invoked"
+
+  set nstr [string trimright $str =]
+  set dstr [string map {
+    A 000000 B 000001 C 000010 D 000011 E 000100 F 000101
+    G 000110 H 000111 I 001000 J 001001 K 001010 L 001011
+    M 001100 N 001101 O 001110 P 001111 Q 010000 R 010001
+    S 010010 T 010011 U 010100 V 010101 W 010110 X 010111
+    Y 011000 Z 011001 a 011010 b 011011 c 011100 d 011101
+    e 011110 f 011111 g 100000 h 100001 i 100010 j 100011
+    k 100100 l 100101 m 100110 n 100111 o 101000 p 101001
+    q 101010 r 101011 s 101100 t 101101 u 101110 v 101111
+    w 110000 x 110001 y 110010 z 110011 0 110100 1 110101
+    2 110110 3 110111 4 111000 5 111001 6 111010 7 111011
+    8 111100 9 111101 + 111110 / 111111
+  } $nstr]
+  switch [expr [string length $str]-[string length $nstr]] {
+    0 {#nothing to do}
+    1 {set dstr [string range $dstr 0 {end-2}]}
+    2 {set dstr [string range $dstr 0 {end-4}]}
+  }
+  set decodedStr [binary format B* $dstr]
+  log::log debug "b64decode returning $decodedStr"
+  return $decodedStr
+}
+
+# testcl::b64encode --
+#
+# stub for the iRule GLOBAL::b64encode - performs base64 encoding on a string
+#
+# Arguments:
+# string
+#
+# Side Effects:
+# None.
+#
+# Results:
+# base64 encoded string
+#
+# Usage syntax:
+# b64encode <base64 string>
+proc ::testcl::b64encode {str} {
+  log::log debug "GLOBAL::b64encode $str invoked"
+
+  binary scan $str B* bits
+  switch [expr {[string length $bits]%6}] {
+    0 {set tail {}}
+    2 {append bits 0000; set tail ==}
+    4 {append bits 00; set tail =}
+  }
+  set encodedStr [string map {
+    000000 A 000001 B 000010 C 000011 D 000100 E 000101 F
+    000110 G 000111 H 001000 I 001001 J 001010 K 001011 L
+    001100 M 001101 N 001110 O 001111 P 010000 Q 010001 R
+    010010 S 010011 T 010100 U 010101 V 010110 W 010111 X
+    011000 Y 011001 Z 011010 a 011011 b 011100 c 011101 d
+    011110 e 011111 f 100000 g 100001 h 100010 i 100011 j
+    100100 k 100101 l 100110 m 100111 n 101000 o 101001 p
+    101010 q 101011 r 101100 s 101101 t 101110 u 101111 v
+    110000 w 110001 x 110010 y 110011 z 110100 0 110101 1
+    110110 2 110111 3 111000 4 111001 5 111010 6 111011 7
+    111100 8 111101 9 111110 + 111111 /
+  } $bits]$tail
+
+  log::log debug "b64encode returning $encodedStr"
+  return $encodedStr
+}
+
+# testcl::call --
+#
+# stub for the iRule GLOBAL::call
+proc ::testcl::call args {
+  return [eval $args]
+}
+
+# testcl::decode_uri --
+#
+# stub for the iRule GLOBAL::decode_uri
+proc ::testcl::decode_uri uri {
+  return [URI::decode $uri]
+}
+
+# testcl::domain --
+#
+# stub for the iRule GLOBAL::domain
+proc ::testcl::domain {fqdn count} {
+  if {($fqdn ne "") && ($count ne "") && ([string is digit $count])} {
+    log::log debug "GLOBAL domain invoked 'domain $fqdn $count'"
+    set domainList [split $fqdn "."]
+    return [join [lrange $domainList [expr {[llength $domainList] - $count}] end] "."]
+  } else {
+    log::log error "GLOBAL domain invoked with incorrect syntax 'domain $fqdn $count', usage domain <string> <count>"
+  }
+}
 
 # testcl::getfield --
 #
--- test/test_irule_variables.tcl
+++ test/test_irule_variables.tcl
@@ -1,16 +1,28 @@
 source src/on.tcl
 source src/assert.tcl
 source src/onirule.tcl
+source src/it.tcl
+source src/irulehttp.tcl
 namespace import ::testcl::*
 
 # Comment out to suppress logging
 #log::lvSuppressLE info 0
 
-event HTTP_REQUEST
+it "should allow access to variables using run variable arg" {
+  event HTTP_REQUEST
+  run irules/variables_irule.tcl simple { status 1 }
+  endstate pool foo
+}
 
-on pool bar return ""
+it "should allow access to variables using setvar" {
+  setvar testVar 5
+  event HTTP_REQUEST
+  run irules/variables_irule.tcl simple
+  endstate pool foo
+}
 
-endstate pool foo
-
-array set variables { status 1 }
-run irules/variables_irule.tcl simple variables
+it "should test irule without variables set" {
+  event HTTP_REQUEST
+  run irules/variables_irule.tcl simple
+  endstate pool bar
+}
